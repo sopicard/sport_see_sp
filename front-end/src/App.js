edd5af7c09c8dd75d5b101c80652e387661aa3fd
sopicard <sogautrelet@gmail.com>
2023-11-10 12:52:56 +0100

    error-message

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -20,34 +20,51 @@ function App() {
   const [dailyActivity, setDailyActivity] = useState(null);
   const [averageSessions, setAverageSessions] = useState(null);
   const [performanceData, setPerformanceData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const useMock = process.env.REACT_APP_useMock === "true";
+
     const fetchUserData = async () => {
-      const response = await getUserById(id, useMock);
-      let userData = response.data;
-    
-      if (!("todayScore" in userData)) {
-         // Utilise la valeur de "score" si "todayScore" n'est pas présent
-        userData = { ...userData, todayScore: userData.score };
+      try {
+        const response = await getUserById(id, useMock);
+        let userData = response.data;
+      
+        if (!("todayScore" in userData)) {
+          // Utilise la valeur de "score" si "todayScore" n'est pas présent
+          userData = { ...userData, todayScore: userData.score };
+        }
+        setUserData(userData);
+      } catch (err) {
+        setError(err.message);
       }
-    
-      setUserData(userData);
     };
     
     const fetchDailyActivity = async () => {
-        const activityData = await getActivityById(id, useMock);
+      try {
+        const activityData = await getActivityById(id, useMock);       
         setDailyActivity(activityData);
+      } catch (err) {
+        setError(err.message);
+      } 
     };
 
     const fetchAverageSessions = async () => {
+      try {
         const sessionsData = await getAverageSessionsById(id, useMock);
         setAverageSessions(sessionsData);
+      } catch (err) {
+        setError(err.message);
+      } 
     };
 
     const fetchPerformanceData = async () => {
+      try {
         const perfData = await getPerformanceById(id, useMock);
         setPerformanceData(perfData);
+      } catch (err) {
+        setError(err.message);
+      } 
     };
 
     fetchUserData();
@@ -56,30 +73,39 @@ function App() {
     fetchPerformanceData();
 }, [id]);
  
-
   return (
     <div className="main-container">
-      <NavTop />
-      <NavLeft />
-      {userData && <Header firstName={userData.userInfos.firstName} />}  
-      <div className="graphics">
-        {dailyActivity && <DailyActivity data={dailyActivity} />}
-        <div className="squares">
-          {averageSessions && <AverageSessions data={averageSessions} />}
-          {performanceData && <Performance data={performanceData} />}
-          {userData && <TodayScore score={userData.todayScore} />}
+      {error ? (
+        <div className="error-message">
+          <p>Oups !...</p>
+          <p>Il semble que les données ne puissent pas être chargées.</p>           
         </div>
-      </div>
-      {userData && (
-        <div className="metrics">
-          <KeyData title="Calories" value={userData.keyData.calorieCount} icon={iconFire} />
-          <KeyData title="Protéines" value={userData.keyData.proteinCount} icon={iconProtein} />
-          <KeyData title="Glucides" value={userData.keyData.carbohydrateCount} icon={iconCarbs} />
-          <KeyData title="Lipides" value={userData.keyData.lipidCount} icon={iconFats} />
-        </div>
-      )}
+      ) : (
+        <>
+          <NavTop />
+          <NavLeft />
+          {userData && <Header firstName={userData.userInfos.firstName} />}  
+          <div className="graphics">
+            {dailyActivity && <DailyActivity data={dailyActivity} />}
+            <div className="squares">
+              {averageSessions && <AverageSessions data={averageSessions} />}
+              {performanceData && <Performance data={performanceData} />}
+              {userData && <TodayScore score={userData.todayScore} />}
+            </div>
+          </div>
+          {userData && (
+            <div className="metrics">
+              <KeyData title="Calories" value={userData.keyData.calorieCount} icon={iconFire} />
+              <KeyData title="Protéines" value={userData.keyData.proteinCount} icon={iconProtein} />
+              <KeyData title="Glucides" value={userData.keyData.carbohydrateCount} icon={iconCarbs} />
+              <KeyData title="Lipides" value={userData.keyData.lipidCount} icon={iconFats} />
+            </div>
+          )}
+        </>
+      )}  
     </div>
   );
-}
+}  
+
 
 export default App;
